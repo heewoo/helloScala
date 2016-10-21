7cc2b5e606293cc4da0758014932dff02cdc9f87
강희우 <heewoo.kang@ysmcorp.com>
2016-10-21 09:42:36 +0900

    list add

---
+++ src/chapter2/function.scala
@@ -0,0 +1,8 @@
+package chapter2
+
+/**
+  * Created by yellomobile on 2016. 10. 5..
+  */
+object function {
+
+}
---
+++ src/chapter3/collections.scala
@@ -0,0 +1,8 @@
+package chapter3
+
+/**
+  * Created by yellomobile on 2016. 10. 18..
+  */
+class collections {
+
+}
